4ebc7623b4bd0d546741f00ff4665b22c9566cd2
Tonya Fletcher <tfletch4504@gmail.com>
2021-06-22 13:53:40 -0400

    formatting

--- the_portfolio/src/pages/About.js
+++ the_portfolio/src/pages/About.js
@@ -3,23 +3,22 @@ import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
 
-
 function About() {
     return (
         <div className="">
-<Container style={{ marginTop: 30 }}>
-    <Row>
-        <Col size="md-12">
-            <h1 className="">About</h1>
-            </Col>
-        </Row>
-        <Row>
-            <Col size="md-12">
+            <Container style={{ marginTop: 30 }}>
+                <Row>
+                    <Col size="md-12">
+                        <h1 className="">About</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size="md-12">
 
-                </Col>
-            </Row>
-    
-    </Container> 
+                    </Col>
+                </Row>
+
+            </Container>
 
         </div>
     );
