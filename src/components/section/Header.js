4f19ca938d355cbb41f595cfb45797a1804073a1
Tonya Fletcher <tfletch4504@gmail.com>
2021-06-24 00:24:00 -0400

    passed in bitmoji to debug image load issue

--- src/components/section/Header.js
+++ src/components/section/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeaderButton from '../layouts/HeaderButton'
 import { header } from '../../profile'
+import bitmoji from '../../styles/images/bitmoji.jpg'
 
 const Header = () => {
 
@@ -38,7 +39,7 @@ const Header = () => {
             </label>
             <HeaderButton/>
             </div>
-            <img id="not-dark" onClick={scrollTo} alt="Contact Me" title="Contact Me" className="gtp" src="profile.jpg"></img>
+            <img id="not-dark" onClick={scrollTo} alt="Contact Me" title="Contact Me" className="gtp" src={bitmoji}></img>
         </div>
     )
     
