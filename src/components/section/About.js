13693f182a1a63297640108265cb9c5f8eb9d4f3
Tonya Fletcher <tfletch4504@gmail.com>
2021-06-28 17:59:41 -0400

    all images responsive

--- src/components/section/About.js
+++ src/components/section/About.js
@@ -1,29 +1,34 @@
 import React from 'react'
 import Skills from '../layouts/Skills'
 import { about, skillsBar, section2title } from '../../profile'
+import compressed from "../../styles/images/compressed.jpg";
+import Container from "../Container";
 
 const About = () => {
     return (
-        <div id="about" className="effect2">
-            <div data-aos="zoom-in-up" data-aos-once="true" className="row">
-            <div className="col-12 d-none d-md-block offset-md-1 col-md-4 Photo" id="not-dark2"></div>
-            <div className="col-12 offset-md-1 col-md-6 about">
-                <div className="About-title-box">
-                <h1 id="About" className="red-line">{section2title}</h1>
+
+        <Container>
+            <div id="about" className="effect2">
+                <div data-aos="zoom-in-up" data-aos-once="true" className="row">
+                    <div className="col-12 offset-md-1 col-md-12 about">
+                        <div className="About-title-box">
+                            <h1 id="About" className="red-line">{section2title}</h1>
+                        </div>
+                        <img src={compressed} alt="profile" />
+                        <p className="lead about-text">
+                            {about.paragraph}
+                        </p>
+                    </div>
                 </div>
-                <p className="lead about-text">
-                    {about.paragraph}  
-                </p>
+                <div id="Skills">
+                    <div className="row d-flex justify-content-center skills">
+                        {skillsBar.map((x) =>
+                            <Skills faClass={x.faClass} label={x.name} />
+                        )}
+                    </div>
+                </div>
             </div>
-            </div>
-            <div id="Skills"> 
-                <div className="row d-flex justify-content-center skills">
-                    {skillsBar.map((x) => 
-                        <Skills faClass={x.faClass} label={x.name}/>
-                    )}
-                </div> 
-            </div>
-        </div>
+        </Container>
     )
 }
 
