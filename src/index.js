481b5c109dcf0c4ec4b3be490a3ec15952de5eef
Tonya Fletcher <tfletch4504@gmail.com>
2023-10-03 22:02:34 -0400

    remove redundant code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/style.css'
 import './styles/images.css'
-// import './styles/no-touch.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
