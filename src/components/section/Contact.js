a699239bc69e4544c23ae4665711c234383e2d85
Tonya Fletcher <tfletch4504@gmail.com>
2023-10-08 19:23:05 -0400

    remove underline

--- src/components/section/Contact.js
+++ src/components/section/Contact.js
@@ -17,7 +17,7 @@ function Contact() {
                     </>
 
                     <div className='center'>
-                                    <p className='navBorder'>_____________________________________________</p>
+                                    <p className='navBorder'>              </p>
                                 </div>
 
 
@@ -30,17 +30,19 @@ function Contact() {
 
                         <div className="d-flex justify-content-center align-items-center flex-column">
                             <div className="inline-block">
+                                <div className="center">
                                 {social.linkedin && <a title="Visit Linkedin profile" rel="noopener noreferrer" target="_blank" href={social.linkedin}><i className="fab fa-linkedin"></i></a>}
                                 {social.facebook && <a title="Visit Facebok profile" rel="noopener noreferrer" target="_blank" href={social.facebook}><i className="fab fa-facebook"></i></a>}
                                 {social.instagram && <a title="Visit Instagram profile" rel="noopener noreferrer" target="_blank" href={social.instagram}><i className="fab fa-instagram"></i></a>}
                                 {social.github && <a title="Visit Github profile" rel="noopener noreferrer" target="_blank" href={social.github}><i className="fab fa-github"></i></a>}
                                 {social.tiktok && <a title="Visit Tiktok profile" rel="noopener noreferrer" target="_blank" href={social.tiktok}><i className="fab fa-tiktok"></i></a>}
                                 {social.resume && <a title="Download Resume" href={social.resume} download><i className="fas fa-download"></i></a>}
+                                </div>
                                 <br />
-
+                                
                                 <>
                                 <div className='center'>
-                                    <p className='navBorder'>_____________________________________________</p>
+                                    <p className='navBorder'>            </p>
                                 </div>
                                     <div className="center">
                                         
